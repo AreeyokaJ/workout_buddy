27564e16d16be10259d3fbec537298c788f86989
Areeyoka John <areeyokajohn@gmail.com>
2025-11-30 20:13:51 -0500

    Added API endpoints that I will later build on

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 require('dotenv').config()
 const express = require('express') 
+const workoutRoutes = require('./routes/workouts')
 
 // express app
 const app = express()
@@ -13,12 +14,13 @@ app.use((req, res, next) => {
 })
 
 // routes 
-app.get('/', (req, res) => {
-    res.json({mssg: "Welcome to the app"})
-})
-
+app.use('/api/workouts', workoutRoutes)
 
 //listen for requests 
 app.listen(process.env.PORT, () => {
     console.log(`listening on port ${PORT}`)
 })
+
+
+
+

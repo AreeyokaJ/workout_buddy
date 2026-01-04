50dec89bfcd056e3910f8a3339cf0b9e4015f6ab
Areeyoka John <areeyokajohn@gmail.com>
2026-01-04 13:22:18 -0500

    Added delete functionality and updated date format

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,4 +1,4 @@
-import {useEffect, useState} from 'react'
+import {useEffect} from 'react'
 import {useWorkoutsContext} from "../hooks/useWorkoutsContext"
 
 //components 
@@ -23,7 +23,7 @@ const Home = () =>{
         }
 
         fetchWorkouts()
-    }, [])
+    }, [dispatch])
 
 
     return (
